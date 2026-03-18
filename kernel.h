fe22816699275d1e65cd28b378cfcef67b4d3e9b
Elif Nur Dagitmac <elifdagitmac3@gmail.com>
2026-03-18 22:47:07 +0300

    kernel.h dosyası tamamlandı.

--- kernel.h
+++ kernel.h
@@ -5,7 +5,7 @@
 #include <stdint.h> 
 // Compilerin uint8_t tipini tanıması için kullanmamız gerekli. (Bellek tasarrufu için bu tipi kullandık.)
 
-typedef void (*GorevFonksiyonu) ():
+typedef void (*GorevFonksiyonu) ();
 // Bu satır sayesinde sisteme eklenecek her görevin aynı tipte olmasını sağladık. (Standartlaştırma yaptık.)
 // Bu parametre almayan, değer döndürmeyen bir fonksiyon olarak tanımlanmıştır. 
 
@@ -17,5 +17,21 @@ struct TCB {
 
 
 };
+
+void nav_core_init(); //Programın başında 1 kez çalışır sadece, sistemin ihtiyaç duyduğu hazırlıkları yapar.
+void gorev_ekle (struct TCB *eklenecek_gorev_adresi); /*Muhtemelen diğer dosyaların içerisinde var olan bir görevin adresine alıp 
+Lİnked List yapısında uygun bir yere ekler.*/
+void scheduler(); //Görevler arasındaki geçiş yönetimini yapan fonksiyon.
+
+//extern dosyaları hafızada yer kaplamaz sadece dosyalara değişkenleri ilan eder.
+extern struct TCB *head;/* Linked List yapısındaki başlangıç fonksiyonunun adres etiketi. 
+Asıl görev başka dosyada burada sadece başlangıç adresini tüm dosyalarla paylaşıyoruz.*/
+extern struct TCB *su_an_calisan; /*Sistemin o anda çalıştırdığı fonksiyonun adresini tüm dosyalarla paylaşan fonksiyon.*/
+
+#endif
+
+
+
+
 
 
